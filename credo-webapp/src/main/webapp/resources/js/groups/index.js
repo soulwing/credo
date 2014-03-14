a31c28748e4e470adc1ebeac0c2e6db80e4314bc
Carl Harris <ceharris@vt.edu>
2014-03-14 16:42:11 -0400

    disable sorters/filters on columns for which they're not appropriate

--- credo-webapp/src/main/webapp/resources/js/groups/index.js
+++ credo-webapp/src/main/webapp/resources/js/groups/index.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
 	    widthFixed: true,
 
 	    headerTemplate : '{content} {icon}', // new in v2.7. Needed to add the bootstrap icon!
-
+	    
 	    // widget code contained in the jquery.tablesorter.widgets.js file
 	    // use the zebra stripe widget if you plan on hiding any rows (filter widget)
 	    widgets : [ "uitheme", "filter" ],
