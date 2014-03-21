238f4c75081e7054985d53cbbc10fbedb0211f40
Carl Harris <ceharris@vt.edu>
2014-03-21 15:05:36 -0400

    continued work on request creation views

---
+++ credo-webapp/src/main/webapp/resources/js/requests/create/success.js
@@ -0,0 +1,11 @@
+$(document).ready(function() { 
+	var $textarea = $("#certificationRequest");
+	$textarea.on("focus", function(event) { 
+		$(this).select();
+		$textarea.on("mouseup", function(event) { 
+			$textarea.off("mouseup");
+			return false;
+		});
+		return false;
+	});
+});
